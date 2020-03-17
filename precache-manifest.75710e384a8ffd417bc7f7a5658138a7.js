f388a3144a83ad1e582dc264bcfb5094c0fe762a
Alex <alexpepke@Alexs-MacBook-Pro.local>
2020-03-16 20:32:02 -0500

    Updates

---
+++ precache-manifest.75710e384a8ffd417bc7f7a5658138a7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "990b64922c9f5d563570ce95173906d1",
+    "url": "/stocks-app/index.html"
+  },
+  {
+    "revision": "e7df8d7e87fdb61e07ff",
+    "url": "/stocks-app/static/css/2.786cb006.chunk.css"
+  },
+  {
+    "revision": "e7df8d7e87fdb61e07ff",
+    "url": "/stocks-app/static/js/2.ff44b374.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/stocks-app/static/js/2.ff44b374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cf3a6454d41805b2683",
+    "url": "/stocks-app/static/js/main.a8195090.chunk.js"
+  },
+  {
+    "revision": "27ab9ec29591fdb5ee73",
+    "url": "/stocks-app/static/js/runtime-main.86978e02.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/stocks-app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/stocks-app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/stocks-app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/stocks-app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/stocks-app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
